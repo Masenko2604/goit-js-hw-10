93a8df7ef994870a7d6fdbb58b26611750af0671
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2023-12-02 20:35:09 +0100

    t-10

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,33 +1,26 @@
 import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] =
-  "live_fcdyt8YrFZOZ5aTPKJ7zw7Pi1zuuCOi0kTqLe0LB44ST5hrr0Fp4jOwI8rWkofTd"
+  'live_fcdyt8YrFZOZ5aTPKJ7zw7Pi1zuuCOi0kTqLe0LB44ST5hrr0Fp4jOwI8rWkofTd';
 
+function fetchBreeds() {
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => response.data)
+    .catch(error => {
+      console.log(error);
+      throw error;
+    });
+}
 
-// const url1 = 'https://api.thecatapi.com/v1/breeds';
-// const url2 = 'https://api.thecatapi.com/v1/images';
+function fetchCatByBreed(breedId) {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => response.data)
+    .catch(error => {
+      console.log(error);
+      throw error;
+    });
+}
 
-// //Унікальний ключ доступу до Cat API
-// const KEY =
-//   "live_fcdyt8YrFZOZ5aTPKJ7zw7Pi1zuuCOi0kTqLe0LB44ST5hrr0Fp4jOwI8rWkofTd";
-
-// //Функція, що фетчить список усіх порід котів
-// fetchBreeds = ("https://api.thecatapi.com/v1/images/0XYvRd7oD")
-//   .then(response => {
-//     return response.json();
-//   })
-//   .then(cat => {
-//     console.log(cat);
-//   });
-// //Функція, що фетчить опис конкретної породи кота по breedId
-// const fetchCatByBreed = breedId => {
-//   return fetch(`${url2}/${breedId}?api_key=${KEY}`).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// };
-
-// //Іменований експорт функцій
-// export { fetchBreeds, fetchCatByBreed };
+export { fetchBreeds, fetchCatByBreed };
